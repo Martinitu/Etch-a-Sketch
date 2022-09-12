3340d65c37893f3a1ad7adaf36af19a6e148ca45
Martinitu <martinitu@gmail.com>
2022-09-12 14:26:20 -0500

    16 by 16 grid add and hover effect add

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const container = document.querySelector(".container")
 
+
+
+    for (let i = 1; i <= 256; i++){
+    let newDiv = document.createElement("div");
+    newDiv.classList = "squareDivs";
+    container.appendChild(newDiv)
+    }
+
+    
+      
+
+  const divs =  document.querySelectorAll(".squareDivs")  
+  console.log(divs)
+ 
+  divs.forEach(element => { 
+   
+    element.addEventListener('mouseenter', e => element.classList.remove("squareDivs"))
+     element.addEventListener('mouseleave', e => element.classList.add("squareDivsChange"))
+      
+  });
