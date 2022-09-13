de957aedb52443d69ce2fc6b928faf66fbc94f8e
Martinitu <martinitu@gmail.com>
2022-09-12 20:06:32 -0500

    some bugs fixed

--- script.js
+++ script.js
@@ -10,19 +10,26 @@ const container = document.querySelector(".container")
  
     function promtColums(){
         var userColumns = prompt("Please enter the number of Colums");
+
+        if (userColumns > 100 || userColumns < 0 || isNaN(userColumns))
+        {
+            alert("please enter a number between 1 and 100")
+        } else {
         numberOfColums =+ userColumns;
          numberOfDivs =  numberOfColums * numberOfColums
         console.log(numberOfColums)
-        createDivs()
+        createDivs(numberOfDivs)
+        }
     }
    
-   function createDivs() {
+   function createDivs(numberOfDivs) {
        for (let i = 1; i <= numberOfDivs; i++){
         console.log(numberOfColums)
     let newDiv = document.createElement("div");
-    newDiv.style.height = divHeight;
+    newDiv.style.heigth = divHeight;
     newDiv.style.width = divHeight;
-    container.style["grid-template-columns"] = "repeat(" + numberOfColums + ", 1fr)"; 
+    container.style["grid-template-columns"] = "repeat(" + numberOfColums + ", 1fr)";
+    container.style["grid-template-rows"] = "repeat(" + numberOfColums + ", 1fr)"; 
     newDiv.classList = "squareDivs";
     container.appendChild(newDiv)
     }
