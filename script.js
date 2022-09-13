bb3271a78222a9604850dcd6d86c8a39fd5f445a
Martinitu <martinitu@gmail.com>
2022-09-12 19:47:32 -0500

    grid  button and hover working

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
+
+
+
 const container = document.querySelector(".container")
 
-
-
-    for (let i = 1; i <= 256; i++){
+    var numberOfColums = 16
+    var numberOfDivs =  numberOfColums * numberOfColums
+    var divHeight = 1000 / numberOfColums
+   
+ 
+    function promtColums(){
+        var userColumns = prompt("Please enter the number of Colums");
+        numberOfColums =+ userColumns;
+         numberOfDivs =  numberOfColums * numberOfColums
+        console.log(numberOfColums)
+        createDivs()
+    }
+   
+   function createDivs() {
+       for (let i = 1; i <= numberOfDivs; i++){
+        console.log(numberOfColums)
     let newDiv = document.createElement("div");
+    newDiv.style.height = divHeight;
+    newDiv.style.width = divHeight;
+    container.style["grid-template-columns"] = "repeat(" + numberOfColums + ", 1fr)"; 
     newDiv.classList = "squareDivs";
     container.appendChild(newDiv)
     }
-
     
-      
-
-  const divs =  document.querySelectorAll(".squareDivs")  
-  console.log(divs)
- 
-  divs.forEach(element => { 
+    const divs =  document.querySelectorAll(".squareDivs")  
+    divs.forEach(element => { 
    
     element.addEventListener('mouseenter', e => element.classList.remove("squareDivs"))
      element.addEventListener('mouseleave', e => element.classList.add("squareDivsChange"))
       
   });
+}
+    
